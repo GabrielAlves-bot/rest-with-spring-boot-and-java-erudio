6ed557efef24aa2cb22251e13713cd85a04b6dcf
Gabriel Alves <gabrielalves00df@gmail.com>
2022-07-19 21:45:57 -0300

    Implementação o Swagger

---
+++ 15_ImplementandoSwagger/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO `person` VALUES (3,'Qnp 15 conjunto j','Gabriel','Male','Paulo'),(4,'Brasilia','Parafuso','Male','Paulo'),(5,'Brasilia','Parafuso','Male','Paulo'),(6,'Brasilia','Parafuso','Male','Paulo'),(7,'Brasilia','Parafuso','Male','Paulo'),(8,'Brasilia','Parafuso','Male','Paulo'),(9,'Qnp 15 conjunto j','Gabriel','Male','Paulo');
