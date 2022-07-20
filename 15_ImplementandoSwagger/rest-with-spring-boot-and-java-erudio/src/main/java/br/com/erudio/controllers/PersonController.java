9e88507e6e4d86fe29c7365e61639a9fcd04865e
Gabriel Alves <gabrielalves00df@gmail.com>
2022-07-19 22:25:13 -0300

    Implementação o Swagger

--- 15_ImplementandoSwagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 15_ImplementandoSwagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,7 +53,7 @@ public class PersonController {
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
-	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}",
 		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
 	@Operation(summary = "Finds a Person", description = "Finds a Person",
@@ -71,7 +72,7 @@ public class PersonController {
 	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
-	
+	@CrossOrigin(origins = { "http://localhost:8080", "http://erudio.com.br"})
 	@PostMapping(
 		consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
 		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
--- 15_ImplementandoSwagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/serialization/converter/YamlJackson2HttpMesageConverter.java
+++ 15_ImplementandoSwagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/serialization/converter/YamlJackson2HttpMesageConverter.java
@@ -11,7 +11,6 @@ public class YamlJackson2HttpMesageConverter extends AbstractJackson2HttpMessage
 	public YamlJackson2HttpMesageConverter() {
 		super(new YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL),
 				MediaType.parseMediaType("application/x-yaml"));
-		// TODO Auto-generated constructor stub
 	}
 
 }
